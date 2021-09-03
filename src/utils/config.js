523fd06a7d5cced5e87b536290bd02ecaefb306e
shaliniarunachalam <shalinieee97@gmail.com>
2021-09-03 22:52:36 +0530

    final commit

---
+++ src/utils/config.js
@@ -0,0 +1,20 @@
+//TODO: add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzV-oxTZoMoWAMM7aZozEGLH1grbXA9ec",
+  authDomain: "mygitapp-6f28f.firebaseapp.com",
+  databaseURL: "https://mygitapp-6f28f-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-6f28f",
+  storageBucket: "mygitapp-6f28f.appspot.com",
+  messagingSenderId: "40910364275",
+  appId: "1:40910364275:web:b93f3b025e64a2c86b1406",
+  measurementId: "G-2GJ7LPJDXB"
+};
